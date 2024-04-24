bd96786a72bf3ff786d7a9026573c5e2f208fbd6
Adhitya Irvansyah <adhitya.irvansyah@bfi.co.id>
2024-04-24 16:35:48 +0700

    refactoring

--- autovote.py
+++ autovote.py
@@ -8,36 +8,46 @@ import json
 
 RFBANANA_CPANEL = "https://cp.rfbanana.ru"
 
+def loadConfigAndSettings():
+    with open('config.json') as configRaw:
+        config = json.load(configRaw)
+        usernames = config['usernames']
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("detach", True)
+    return usernames,chrome_options
+
+def login(driver, username):
+    usernameInput = driver.find_element(By.NAME, "username")
+    usernameInput.clear()
+    usernameInput.send_keys(username)
+    usernameInput.send_keys(Keys.RETURN)
+
+    WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.LINK_TEXT, "Logout")))
+
+def logout(driver):
+    logoutButton = driver.find_element(By.LINK_TEXT, "Logout")
+    logoutButton.click()
+    WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.LINK_TEXT, "Login")))
+
+def tryVoteAllOption(driver):
+    windowHandle = driver.current_window_handle;
+    driver.get(RFBANANA_CPANEL + "/index.php?do=user_vote")
+    voteButtons = driver.find_elements(By.NAME, "vote_id")
+    for button in voteButtons:
+        if(not button.is_enabled()):
+            continue
+        button.click()
+        driver.switch_to.window(windowHandle)
+
 usernames = []
-with open('config.json') as configRaw:
-    config = json.load(configRaw)
-    usernames = config['usernames']
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
+usernames, chrome_options = loadConfigAndSettings()
 
 with webdriver.Chrome(options= chrome_options) as driver:
     print(usernames)
     for username in usernames:
         driver.get(RFBANANA_CPANEL)
-        windowHandle = driver.current_window_handle;
-
-        usernameInput = driver.find_element(By.NAME, "username")
-        usernameInput.clear()
-        usernameInput.send_keys(username)
-        usernameInput.send_keys(Keys.RETURN)
-
-        WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.LINK_TEXT, "Logout")))
-        
-        driver.get(RFBANANA_CPANEL + "/index.php?do=user_vote")
-
-        voteButtons = driver.find_elements(By.NAME, "vote_id")
-        for button in voteButtons:
-            if(not button.is_enabled()):
-                continue
-            button.click()
-            driver.switch_to.window(windowHandle)
-        logoutButton = driver.find_element(By.LINK_TEXT, "Logout")
-        logoutButton.click()
-        WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.LINK_TEXT, "Login")))
+        login(driver, username)
+        tryVoteAllOption(driver)
+        logout(driver)
         print("RF Banana voting for account: " + username + " is complete")
     print("Finish voting for all account inputted")
